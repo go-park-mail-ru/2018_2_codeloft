7efee75d5747351a78a5f83e58d6a39ba4c8b298
Ryazanov Maxim <ryazanovms@yandex.ru>
2018-10-05 18:06:44 +0300

    Add CORS

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 
 	"2018_2_codeloft/database"
 	"2018_2_codeloft/handlers"
+
+	"github.com/rs/cors"
 )
 
 var dataBase *database.DB
@@ -18,12 +20,14 @@ func init() {
 }
 
 func main() {
-	http.HandleFunc("/", handlers.MainPage)
-	http.HandleFunc("/user", handlers.UserHandler)
-	http.HandleFunc("/session", handlers.SessionHandler)
-	http.HandleFunc("/user/", handlers.UserById)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", handlers.MainPage)
+	mux.HandleFunc("/user", handlers.UserHandler)
+	mux.HandleFunc("/session", handlers.SessionHandler)
+	mux.HandleFunc("/user/", handlers.UserById)
 
 	fmt.Println("starting server on http://127.0.0.1:8080")
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", cors.Default().Handler(mux))
 }
