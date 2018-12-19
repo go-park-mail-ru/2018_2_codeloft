ec4b223d6d61463e98c960dcf00356ed8568b72f
Malefaro <malefaro@gmail.com>
2018-12-19 20:03:51 +0300

    FIX

--- to_machine_deploy.sh
+++ to_machine_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export SSHPASS=$DEPLOY_SSH
-sshpass -e ssh -o StrictHostKeychecking=no $DEPLOY_USER@$DEPLOY_HOST "cd $DEPLOY_PATH && ./deploy.sh"
+sshpass -e ssh -o StrictHostKeychecking=no $DEPLOY_USER@$DEPLOY_HOST "cd $DEPLOY_PATH && ./deploy.sh $TRAVIS_BRANCH"
